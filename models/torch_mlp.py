2f73a8f9d0405932a27f0a7f90ce1b1a460fb3fd
alexandrashaw10 <alels4321@gmail.com>
2023-05-20 14:06:30 +0100

    created new MLP suitable for lip constraints

--- models/torch_mlp.py
+++ models/torch_mlp.py
@@ -3,8 +3,55 @@ from typing import Tuple
 import torch
 from torch import nn
 
-from torchrl.modules import MLP
+#from torchrl.modules import MLP
 from monotonenorm import direct_norm
+
+
+class LipNormedMLP(nn.Module):
+    def __init__(self, in_features: int, out_features: int, 
+                 depth: int, num_cells: int, 
+                 activation_class: nn.Module, device: torch.device | str | int | None = None,
+                 lip_constrained: bool = False, sigma: float | None = None):
+        super().__init__()
+
+        # credit to monotonenorm package
+        def lipschitz_norm(module):
+            return direct_norm(
+                module,  # the layer to constrain
+                "one",  # |W|_1 constraint type
+                max_norm=sigma ** (1 / depth),  # norm of the layer (LIP ** (1/nlayers))
+            )
+
+        self.layers = nn.ModuleList()
+
+        # lipschitz constraints are added to the linear layer forward pre-hook 
+        # so that it is normalized before the backward pass and the normalized gradients are passed through
+        if lip_constrained:
+            self.layers.append(lipschitz_norm(nn.Linear(in_features, num_cells)))
+            for d in range(1,depth-1):
+                self.layers.append(lipschitz_norm(nn.Linear(num_cells, num_cells)))
+                if activation_class is not None:
+                    self.layers.append(activation_class)
+            
+            self.layers.append(lipschitz_norm(nn.Linear(num_cells, out_features)))
+            self.layers.append(activation_class)
+        else:
+            self.layers.append(nn.Linear(in_features, num_cells))
+            for d in range(1,depth-1):
+                self.layers.append(nn.Linear(num_cells, num_cells))
+                if activation_class is not None:
+                    self.layers.append(activation_class)
+            self.layers.append(nn.Linear(num_cells, out_features))
+            self.layers.append(activation_class)
+
+        self.device = device
+        self.to(device)
+
+    def forward(self, input_data):
+        # does this need to include something with the normalization to work?
+        for layer in self.layers:
+            input_data = layer(input_data)
+        return input_data
 
 
 class MultiAgentMLP(nn.Module):
@@ -23,14 +70,7 @@ class MultiAgentMLP(nn.Module):
         sigma=1.0,
     ):
         super().__init__()
-        # credit to monotonenorm package
-        def lipschitz_norm(module):
-            return direct_norm(
-                module, # layer to constrain
-                "one", # defines p for the p-norm constraint type
-                max_norm=sigma ** (1/3)
-            )
-
+        
         self.n_agents = n_agents
         self.n_agent_inputs = n_agent_inputs
         self.n_agent_outputs = n_agent_outputs
@@ -39,18 +79,18 @@ class MultiAgentMLP(nn.Module):
 
         self.agent_networks = nn.ModuleList(
             [
-                MLP( # input, hidden, output
+                # switched from using nn.Modules MLP to own MLP implementation
+                LipNormedMLP(
                     in_features=n_agent_inputs
                     if not centralised
                     else n_agent_inputs * n_agents,
                     out_features=n_agent_outputs,
                     depth=depth,
-                    norm_class=lipschitz_norm if lip_constrained else None, # stack a norm layer on top of the linear layer
-					# want to do def lipschitz_norm(module) and return direct_norm from the monotoneNorm module
-					# then can do that
                     num_cells=num_cells,
                     activation_class=activation_class,
                     device=device,
+                    lip_constrained=lip_constrained,
+                    sigma=sigma,
                 )
                 for _ in range(self.n_agents if not self.share_params else 1)
             ]
