106b1b4f5b998ee3c76a61eb1555c79e957cc034
alexandrashaw10 <alels4321@gmail.com>
2023-05-21 16:57:10 +0100

    adding lip normalization into the file directly

--- models/lip_mlp.py
+++ models/lip_mlp.py
@@ -5,7 +5,9 @@ import torch
 from torch import nn
 
 #from torchrl.modules import MLP
-from monotonenorm import direct_norm
+# from monotonenorm import direct_norm
+from torch.nn.utils.parametrize import register_parametrization
+
 
 class LipNormedMLP(nn.Module):
     def __init__(self, in_features: int, out_features: int, 
@@ -15,12 +17,36 @@ class LipNormedMLP(nn.Module):
         super().__init__()
 
         # credit to monotonenorm package
-        def lipschitz_norm(module):
-            return direct_norm(
-                module,  # the layer to constrain
-                "one",  # |W|_1 constraint type
-                max_norm=sigma ** (1 / depth),  # norm of the layer (LIP ** (1/nlayers))
-            )
+        def lipschitz_norm(layer: nn.Linear, always_norm: False, sigma: float | None):
+            '''
+            layer - the nn.Linear layer to normalize
+            always_norm - always normalize the weight matrix to the max_norm if True. Default is False so it can be < max_norm
+            max_norm - the lipschitz constraint on the network
+            '''
+            max_norm = sigma ** (1 / depth) # the norm of each layer
+
+            class LipNormalize(nn.Module):
+                def forward(self, W):
+                    norms = W.abs().sum(axis=0) # compute 1-norm of W
+                    
+                    if not always_norm:
+                        # take the maximum of 1s or the norm / max_norm (which could be larger than 1)
+                        norms = torch.max(torch.ones_like(norms), norms / max_norm)
+                    else:
+                        # otherwise just take the norm divided by the max_norm
+                        norm_W = norm_W / max_norm
+
+                    weight = W / torch.max(norm_W, torch.ones_like(norm_W)*1e-10) # what does this do ?
+
+                    return weight
+            
+            register_parametrization(layer, "weight", LipNormalize())
+            
+            # return direct_norm(
+            #     module,  # the layer to constrain
+            #     "one",  # |W|_1 constraint type
+            #     max_norm=sigma ** (1 / depth),  # norm of the layer (LIP ** (1/nlayers))
+            # )
 
         self.layers = nn.ModuleList()
 
